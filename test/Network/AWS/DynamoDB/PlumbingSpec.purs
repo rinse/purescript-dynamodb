2c2f88743d7eb6a43d856d26cb2b403a6112ebaf
rinse <rinse@neko2.net>
2020-02-22 18:29:58 +0900

    fix documentclient

--- test/Network/AWS/DynamoDB/PlumbingSpec.purs
+++ test/Network/AWS/DynamoDB/PlumbingSpec.purs
@@ -4,14 +4,23 @@ import Prelude
 
 import Control.Monad.Error.Class     (catchError)
 import Data.Maybe                    (Maybe)
+import Effect                        (Effect)
 import Effect.Aff                    (Aff)
 import Effect.Class                  (liftEffect)
 import Effect.Exception              (message)
 import Network.AWS.DynamoDB          (documentClient)
 import Network.AWS.DynamoDB.Plumbing (delete, get, put, scan)
+import Network.AWS.DynamoDB.Types    (DocumentClient)
 import Test.Spec                     (Spec, describe, it)
 import Test.Spec.Assertions          (shouldEqual)
 
+
+-- A documentClient with fake credentials.
+documentClient' :: Effect DocumentClient
+documentClient' = documentClient
+    { accessKeyId: "accessKeyId"
+    , secretAccessKey: "secretAccessKey"
+    }
 
 -- A type of an item.
 type Person = { name :: String, age :: Int }
@@ -24,24 +33,24 @@ spec :: Spec Unit
 spec = do
     describe "get" $
         it "is bound to `AWS.DocumentClient.get`" $ do
-            client <- liftEffect $ documentClient {}
+            client <- liftEffect documentClient'
             verifyMissingRegion $
                 void $ (get client { "TableName": "", "Key": {} } :: Aff (Maybe Person))
 
     describe "delete" $
         it "is bound to `AWS.DocumentClient.delete`" $ do
-            client <- liftEffect $ documentClient {}
+            client <- liftEffect documentClient'
             verifyMissingRegion $
                 delete client { "TableName": "", "Key": {} }
 
     describe "put" $
         it "is bound to `AWS.DocumentClient.put`" $ do
-            client <- liftEffect $ documentClient {}
+            client <- liftEffect documentClient'
             verifyMissingRegion $
                 put client { "TableName": "", "Item": {} }
 
     describe "scan" $
         it "is bound to `AWS.DocumentClient.scan`" $ do
-            client <- liftEffect $ documentClient {}
+            client <- liftEffect documentClient'
             verifyMissingRegion $
                 void $ (scan client { "TableName": "" } :: Aff (Array Person))
--- test/Network/AWS/DynamoDBSpec.purs
+++ test/Network/AWS/DynamoDBSpec.purs
@@ -2,17 +2,26 @@ module Test.Network.AWS.DynamoDBSpec  where
 
 import Prelude
 
-import Control.Monad.Error.Class     (catchError)
-import Effect.Aff                    (Aff)
-import Effect.Class                  (liftEffect)
-import Effect.Exception              (message)
-import Data.Symbol                   (class IsSymbol, SProxy(..))
-import Foreign.Generic.Class         (class Decode)
-import Network.AWS.DynamoDB          (documentClient, runDynamoDB)
-import Network.AWS.DynamoDB.Class    (class DynamoItem, getItem, putItem, removeItem, scanItems)
-import Test.Spec                     (Spec, describe, it)
-import Test.Spec.Assertions          (shouldEqual)
+import Control.Monad.Error.Class  (catchError)
+import Effect                     (Effect)
+import Effect.Aff                 (Aff)
+import Effect.Class               (liftEffect)
+import Effect.Exception           (message)
+import Data.Symbol                (class IsSymbol, SProxy(..))
+import Foreign.Generic.Class      (class Decode)
+import Network.AWS.DynamoDB       (documentClient, runDynamoDB)
+import Network.AWS.DynamoDB.Class (class DynamoItem, getItem, putItem, removeItem, scanItems)
+import Network.AWS.DynamoDB.Types (DocumentClient)
+import Test.Spec                  (Spec, describe, it)
+import Test.Spec.Assertions       (shouldEqual)
 
+
+-- A documentClient with fake credentials.
+documentClient' :: Effect DocumentClient
+documentClient' = documentClient
+    { accessKeyId: "accessKeyId"
+    , secretAccessKey: "secretAccessKey"
+    }
 
 -- A name of a table.
 foreign import data TablePerson :: Symbol
@@ -47,31 +56,31 @@ spec = do
 
     describe "runDynamoDB" $
         it "runs `DynamoDB` and get a result" $ do
-            client <- liftEffect $ documentClient {}
+            client <- liftEffect documentClient'
             runDynamoDB tablePerson client (pure unit)
 
     describe "DynamoDB" $ do
         describe "getItem" $
             it "is bound to `AWS.DocumentClient.get`" $ do
-                client <- liftEffect $ documentClient {}
+                client <- liftEffect documentClient'
                 verifyMissingRegion $
                     void $ runDynamoDB tablePerson client $
                         getItem $ PrimaryKeyPerson { name: "John" }
         describe "putItem" $
             it "is bound to `AWS.DocumentClient.put`" $ do
-                client <- liftEffect $ documentClient {}
+                client <- liftEffect documentClient'
                 verifyMissingRegion $
                     void $ runDynamoDB tablePerson client $
                         putItem $ Person { name: "John", age: 20 }
         describe "removeItem" $
             it "is bound to `AWS.DocumentClient.remove`" $ do
-                client <- liftEffect $ documentClient {}
+                client <- liftEffect documentClient'
                 verifyMissingRegion $
                     void $ runDynamoDB tablePerson client $
                         removeItem $ PrimaryKeyPerson { name: "John" }
         describe "scanItems" $
             it "is bound to `AWS.DocumentClient.scan`" $ do
-                client <- liftEffect $ documentClient {}
+                client <- liftEffect documentClient'
                 verifyMissingRegion $
                     void $ runDynamoDB tablePerson client $
                         scanItems
